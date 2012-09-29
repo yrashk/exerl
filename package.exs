0c0df241ab295fe2382e149c49efa84a0c64de55
Yurii Rashkovskii <yrashk@gmail.com>
2012-09-29 03:38:45 -0700

    Add expm package.exs

---
+++ package.exs
@@ -0,0 +1,4 @@
+Expm.Package.new(name: "exerl", description: "Elixir macro to alias Elixir module names in Erlang",
+                 version: "0.0.1", keywords: ["Elixir","Erlang","module"], 
+                 maintainers: [[name: "Yurii Rashkovskii", email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/exerl", tag: "0.0.1"]])
